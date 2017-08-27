dd21530308a35e67efd554e72bac9de7f16978f6
Ilan Rosen <ilanbrianrosen@gmail.com>
2017-08-27 15:40:48 +1000

    overriding diamondsquare with mine, mine has some colour, and a public pointlight variable

--- graphicsproject1/Assets/DiamondSquareTerrain.cs
+++ graphicsproject1/Assets/DiamondSquareTerrain.cs
@@ -8,8 +8,10 @@ public class DiamondSquareTerrain : MonoBehaviour
 	public int mDivisions;				// Must be powers of 2. Default & max is 128.
 	public static float mSize=10000;		// 100 to 10,000. Default is 10,000.
 	public float iterRate;				// Between 0 and 1. Default is 0.5.
+    public PointLight pointLight;       //Light source, here will mimick the sun
 
-	Vector3[] mVerts;
+
+    Vector3[] mVerts;
 	Color[] mColours;
 	int mVertCount;
 
@@ -24,7 +26,18 @@ public class DiamondSquareTerrain : MonoBehaviour
 
 	}
 
-	void GenerateTest()
+    void Update()
+    {
+        // Get renderer component (in order to pass params to shader)
+        MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+
+        // Pass updated light positions to shader
+        renderer.material.SetColor("_PointLightColor", this.pointLight.color);
+        renderer.material.SetVector("_PointLightPosition", this.pointLight.GetWorldPosition());
+    }
+
+
+    void GenerateTest()
 	{
 
 		float mHeight = mSize / 4.0f; // Sets the height of the mesh. Proportional to mesh size.
@@ -166,7 +179,7 @@ public class DiamondSquareTerrain : MonoBehaviour
 		mesh.vertices = mVerts;
 		mesh.uv = UVs;
 		mesh.triangles = tris;
-		mesh.colors = mColours;
+		//mesh.colors = mColours;
 
 		mesh.RecalculateBounds();
 		mesh.RecalculateNormals();
